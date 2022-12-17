715f1c206dd0e42644415e2f4cd586ba392b3f12
andvigo <animacion96@gmail.com>
2022-12-18 00:22:34 +0100

    mostrando el problema

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 public class Main {
     public static void main(String[] args) {
-        
+
         //Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         //p.edad = -31; // Arreglado: error de compilación. edad no es accesible
 
@@ -21,6 +21,8 @@ public class Main {
         System.out.println(p2);
          */
 
+        System.out.println("Si quiero mostrar solo el nombre: " + p.nombre);
+        System.out.println("Si quiero mostrar solo la edad no puedo: " + p.edad); // necesito un getter
     }
 }
 
