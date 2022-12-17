f3c8b9343872b21c13da4ea25eedf8de7c54ce89
andvigo <animacion96@gmail.com>
2022-12-17 23:20:52 +0100

    probando objeto empleado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 /**
  * @Author: Andrés Fernández Pereira
  */
+import java.util.Calendar;
+import java.util.Date;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
+        System.out.println("Empleado: " + p.nombre + " (nacido en 19" + p.birthDate.getYear() + ")");
     }
 }
+ 
