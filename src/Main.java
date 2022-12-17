72425a3763dcc3b18874139c9adab74e7fe3c542
Andres <animacion96@gmail.com>
2022-12-17 12:56:49 +0100

    programa que crea y muestra un objeto empleado

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 /**
  * @Author: Andrés Fernández Pereira
  */
+import java.util.Calendar;
+import java.util.Date;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
+        System.out.println(p);
     }
 }
