2332593af2f468c15d3d9c65826e6955e63a1d6c
andvigo <animacion96@gmail.com>
2022-12-18 00:37:44 +0100

    migrado a LocalDate

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,16 +1,17 @@
 /**
  * @Author: Andrés Fernández Pereira
  */
+import java.time.LocalDate;
 import java.util.Date;
 
 class Empleado {
 
    final String nombre;
     private int edad;
-   private final Date fechaContratacion;
+   private final LocalDate fechaContratacion;
 
 
-    private Empleado(String nombre, int edad, Date fechaContratacion) {
+    private Empleado(String nombre, int edad, LocalDate fechaContratacion) {
         this.nombre = nombre;
         this.edad = edad;
         this.fechaContratacion = fechaContratacion;
@@ -25,7 +26,7 @@ class Empleado {
         return edad;
     }
 
-    public Date getFechaContratacion() {
+    public LocalDate getFechaContratacion() {
         return fechaContratacion;
     }
 
@@ -38,7 +39,7 @@ class Empleado {
      * Método factoría que permite comprobar si la edad es válida antes de llamar al constructor.
      * El constructor es ahora privado, impidiendo llamarlo directamente desde otra clase.
      */
-    public static Empleado crearEmpleado(String nombre, int edad, Date fechaContratacion) {
+    public static Empleado crearEmpleado(String nombre, int edad, LocalDate fechaContratacion) {
         if (edad < 0) throw new RuntimeException("Un empleado no puede tener edad negativa");
         else return new Empleado(nombre, edad, fechaContratacion);
     }
