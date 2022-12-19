26e700db57914cf0412131be2f70606647dbd5b8
Andres <animacion96@gmail.com>
2022-12-19 15:08:20 +0100

    probando paso por valor y referencia

---
+++ src/Clase.java
@@ -0,0 +1,22 @@
+/**
+ * @Author: Andrés Fernández Pereira
+ */
+class Clase {
+    int i;
+    String string = "string1";
+
+    public Clase(int i, String string) {
+        this.i = i;
+        this.string = string;
+    }
+
+    public void modificaObjeto(int nuevo_int, String nuevo_string) {
+        i = nuevo_int;
+        string = nuevo_string;
+    }
+
+    @Override
+    public String toString() {
+        return "Objeto { i=" + i + ", a='" + string + "\'}";
+    }
+}
